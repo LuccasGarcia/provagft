b06e19e95c15293e80cde15bff0d538a22ba5654
LuccasGarcia <garcia.luccas@gmail.com>
2021-08-23 16:07:17 -0300

    commit inicial

---
+++ provagft/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace provagft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
